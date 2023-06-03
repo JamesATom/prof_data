d634f981cb1a21981c34edeef13770a650f8b3e4
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-06-03 12:33:20 +0500

    what

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const router = require('./routes/endPoints');
+const router = require('./routes/routes');
 
 app.use(express.json());
 app.use(cors());
