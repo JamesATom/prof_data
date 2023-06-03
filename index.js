f0906587f4c1a86d0e232eea549ee41b2f3216ef
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-06-03 12:27:43 +0500

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const router = require('./routes/endPoints');
+
+app.use(express.json());
+app.use(cors());
+app.use('/user', router)
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log('Listening on Port');
+});
